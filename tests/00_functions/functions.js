6832c02c3c4274880e60ada262c3542f73177313
Disney Harley <disneyharley26@gmail.com>
2022-12-09 20:49:42 -0500

    completed all questions max,addunknown,evenorodd,operator

--- tests/00_functions/functions.js
+++ tests/00_functions/functions.js
@@ -1,19 +1,40 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable consistent-return */
-function max() {
-
+function max(n1, n2, n3) {
+  let result = n1
+  if (n2 > n1) {
+    result = n2
+  }
+  if (n3 > n1) {
+    result = n3
+  }
+  return result
 }
 
-function addUnknown() {
-
+function addUnknown(...nums) {
+  let result = 0
+  for (let i = 0; i < nums.length; i++) {
+    result += nums[i]
+  }
+  return result
 }
 
-function evenOrOdd() {
-
+function evenOrOdd(number) {
+  return number % 2 === 0 ? 'even' : 'odd'
 }
 
-function operator() {
-
+function operator(n1, n2, n3) {
+  if (n1 + n2 === n3) {
+    return 'plus'
+  } else if (n1 - n2 === n3) {
+    return 'minus'
+  } else if (n1 * n2 === n3) {
+    return 'multiply'
+  } else if (n1 / n2 === n3) {
+    return 'divide'
+  } else {
+    return null
+  }
 }
 
 module.exports = {
