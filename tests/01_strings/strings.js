9ea43aeb22c1078e910191ae6a22492a5125d29a
Disney Harley <disneyharley26@gmail.com>
2022-12-12 21:15:53 -0500

    competed cap,firstcharacter,doesinclude

--- tests/01_strings/strings.js
+++ tests/01_strings/strings.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 
 function doesInclude(str, letter) {
-
+  return str.includes(letter) ? true : false
 }
 
 function isPalindrome(str) {
@@ -9,11 +9,17 @@ function isPalindrome(str) {
 }
 
 function cap(str, letter) {
-
+  if (str.includes(letter)) {
+    return str.substring(str.indexOf(letter) + 1, str.indexOf(letter) + 2).toUpperCase()
+  }
+  return 'sorry not found'
 }
 
 function firstCharacter(str1, str2) {
-
+  if (str1.substring(0,1).toLowerCase() === str2.substring(0,1).toLowerCase()) {
+    return true
+  }
+  return false
 }
 
 module.exports = {
