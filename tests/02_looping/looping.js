09fc4889f84f3d05681d6fb3dee8e8fb62e61184
Disney Harley <disneyharley26@gmail.com>
2022-12-16 10:16:25 -0500

    completed factorial, stringreverse, addtheevens

--- tests/02_looping/looping.js
+++ tests/02_looping/looping.js
@@ -1,11 +1,21 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 function factorial(num) {
-
+  let i = 1
+  let total = 1
+  while (i <= num) {
+    total *= i
+    i++
+  }
+  return total
 }
 
 function stringReverse(str) {
-
+  let output = []
+  for (let i = str.length - 1; i >= 0; i--) {
+    output.push(str[i])
+  }
+  return output.join('')
 }
 
 // do not use built in slice method.
@@ -14,7 +24,13 @@ function slicer(originalString, startIdx, endIdx) {
 }
 
 function addTheEvens(num) {
-
+  let total = 0
+  for (let i = 0; i <= num; i++) {
+    if (i % 2 === 0) {
+      total += i
+    }
+  }
+  return total
 }
 
 module.exports = {
